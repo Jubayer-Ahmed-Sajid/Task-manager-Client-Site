79d6b3eb298b453e36362fc7c4ca40a7ee23977c
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-12-24 11:34:56 +0600

    private route created

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,29 @@
+import { Navigate} from "react-router-dom";
+import useAuth from "../Components/hooks/useAuth";
+import PropTypes from 'prop-types'
+
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useAuth()
+    if (loading) {
+        return (
+            <div className="h-screen flex items-center justify-center">
+                <span className="loading loading-spinner text-error"></span>
+            </div>
+        )
+    }
+    else if (user?.email) {
+        return (
+            children
+        )
+    }
+    else {
+         return <Navigate to='/login' replace></Navigate>
+    }
+
+};
+PrivateRoute.propTypes ={
+    children: PropTypes.node
+}
+
+export default PrivateRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Registration from "../Pages/Authorization/Registration/Registration";
 import Login from "../Pages/Authorization/Login/Login";
 import Home from "../Pages/Home/Home";
 import UpdateTask from "../Pages/Home/Components/UpdateTask";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
     {
@@ -17,7 +18,7 @@ export const router = createBrowserRouter([
             },
             {
                 path:'/',
-                element:<Home></Home>
+                element:<PrivateRoute><Home></Home></PrivateRoute>
             },
             {
                 path:'login',
@@ -25,7 +26,7 @@ export const router = createBrowserRouter([
             },
             {
                 path:'tasks/:id',
-                element:<UpdateTask></UpdateTask>,
+                element:<PrivateRoute><UpdateTask></UpdateTask></PrivateRoute>,
                 loader:({params})=> fetch(`http://localhost:5000/todo/${params.id}`)
             }
         ]
